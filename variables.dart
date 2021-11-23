a42f8a45d8d384da95a321fb4d34b6d3b8773799
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-11-24 01:12:15 +0600

    variables

---
+++ variables.dart
@@ -0,0 +1,8 @@
+// dart:core package is imported in every program automatically. It contains basic dart functionalities.
+
+void main(List<String> args) {
+  var firstName = 'Muntaha'; // Type inference.
+  String lastName = 'Islam';
+
+  print(firstName + ' ' + lastName);
+}
