cdda8515d2014bdaa698c6987c5518ff97a56eec
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-11-24 01:34:13 +0600

    data_types

---
+++ 5 - data_types.dart
@@ -0,0 +1,31 @@
+void main(List<String> args) {
+  /*
+  int
+  double
+  String
+  bool
+  dynamic -> run time
+  */
+
+  int a = 10;
+  var b = 20;
+  print('a = $a and b = $b');
+
+  double c = 10.5;
+  var d = 20.3;
+  print('c = $c and d = $d');
+
+  String e = 'a string';
+  var f = 'another string';
+  print('e = $e and f = $f');
+
+  bool g = true;
+  var h = false;
+  print('g = $g and h = $h');
+
+  dynamic weakVariable = 100;
+  print('weakVariable: $weakVariable');
+
+  weakVariable = "Now it's a String.";
+  print('weakVariable: $weakVariable');
+}
